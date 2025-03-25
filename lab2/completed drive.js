1bed9d126cbdbcffb46ffa225cb84901ac1fb61e
Mohamed Farghly <111466680+MOHAMEDFARGHLY@users.noreply.github.com>
2025-03-25 23:11:45 +0200

    completed drive.js

--- lab2/completed drive.js
+++ lab2/completed drive.js
@@ -5,12 +5,9 @@ let { blinded, r } = blind(doc, agency.n, agency.e);
 blindedDocs.push(blinded);
 blindingFactors.push(r);
 }
-
 agency.signDocument(blindedDocs, (selected, verifyAndSign) => {
 let blindingFactorsForVerification = [];
 let originalDocsForVerification = [];
-
-// Populate arrays for verification, skipping the selected document
 for (let i = 0; i < 10; i++) {
   if (i === selected) {
     blindingFactorsForVerification.push(undefined);
@@ -20,21 +17,15 @@ for (let i = 0; i < 10; i++) {
     originalDocsForVerification.push(documents[i]);
   }
 }
-
-// Call verifyAndSign function
 let blindedSignature = verifyAndSign(
   blindingFactorsForVerification,
   originalDocsForVerification
 );
-
-// Unblind the signature for the selected document
 let unblindedSignature = unblind(
   blindingFactors[selected],
   blindedSignature,
   agency.n
 );
-
-// Validate the signature
 let isValid = blindSignatures.verify({
   unblinded: unblindedSignature,
   message: documents[selected],
